5e2ab812c7593838440bdcd13a574a7e27d5d70d
luchanghe <757864997@qq.com>
2024-08-23 17:24:28 +0800

    管理器统一调用包改为manage

--- server/pkg/manage/user_test.go
+++ server/pkg/manage/user_test.go
@@ -32,7 +32,7 @@ func TestUserCp(t *testing.T) {
 	u.Name = "firstName"
 	u.Hero.HeroId = 1000
 	u.Props[10] = &model.Prop{PropId: 10, PropNum: 10}
-	fmt.Println("对象池", GetManage().UserManage.users[100001], "上下文中的原始数据", updateUserMap.(*UpdateUsersStruct).userWatcher[100001].originalUser, "上下文中的当前数据", updateUserMap.(*UpdateUsersStruct).userWatcher[100001].currentUser, "二次获得", u2)
+	fmt.Println("对象池", GetUserManage().users[100001], "上下文中的原始数据", updateUserMap.(*UpdateUsersStruct).userWatcher[100001].originalUser, "上下文中的当前数据", updateUserMap.(*UpdateUsersStruct).userWatcher[100001].currentUser, "二次获得", u2)
 }
 
 type userTestStruct struct {
