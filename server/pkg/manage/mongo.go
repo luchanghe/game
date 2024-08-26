9f7321f1da8712c5df31a5e38f2d62b26403684b
luchanghe <757864997@qq.com>
2024-08-26 17:16:06 +0800

    mongoDb使用连接池

--- server/pkg/manage/mongo.go
+++ server/pkg/manage/mongo.go
@@ -2,7 +2,6 @@ package manage
 
 import (
 	"context"
-	"go.mongodb.org/mongo-driver/event"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -20,17 +19,11 @@ var mongoManageCache *MongoManage
 
 func GetMongoManage() *MongoManage {
 	mongoManageOnce.Do(func() {
-		poolMonitor := &event.PoolMonitor{
-			Event: func(evt *event.PoolEvent) {
-				log.Printf("connection pool event: %s\n", evt)
-			},
-		}
 		url := strings.Join([]string{"mongodb://", GetConfigManage().GetString("mongo.host"), ":", GetConfigManage().GetString("mongo.port")}, "")
 		option := options.Client().ApplyURI(url)
 		option = option.SetMaxPoolSize(GetConfigManage().GetUint64("mongo.max_pool_size"))
-		option = option.SetPoolMonitor(poolMonitor)
 		option = option.SetMinPoolSize(GetConfigManage().GetUint64("mongo.min_pool_size"))
-		option = option.SetMaxConnIdleTime(time.Second * time.Duration(GetConfigManage().GetUint64("mongo.max_conn_minute")))
+		option = option.SetMaxConnIdleTime(time.Minute * time.Duration(GetConfigManage().GetUint64("mongo.max_conn_minute")))
 		var err error
 		mongoManageCache.Client, err = mongo.Connect(context.TODO(), option)
 		if err != nil {
