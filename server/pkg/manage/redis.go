b26cf4b4d3872ab6cb924e8de12eca12272d62c3
luchanghe <757864997@qq.com>
2024-09-02 20:39:12 +0800

    增加redis支持

---
+++ server/pkg/manage/redis.go
@@ -0,0 +1,30 @@
+package manage
+
+import (
+	"github.com/go-redis/redis/v8"
+	"sync"
+	"time"
+)
+
+type RedisManage struct {
+	Client *redis.Client
+}
+
+var redisManageOnce sync.Once
+var redisManageCache *RedisManage
+
+func GetRedisManage() *RedisManage {
+	redisManageOnce.Do(func() {
+		redisManageCache.Client = redis.NewClient(&redis.Options{
+			Addr:         GetConfigManage().GetString("redis.host") + ":" + GetConfigManage().GetString("redis.port"),
+			Password:     GetConfigManage().GetString("redis.password"),
+			DB:           GetConfigManage().GetInt("redis.db"),
+			PoolSize:     GetConfigManage().GetInt("redis.pool_size"),
+			MinIdleConns: GetConfigManage().GetInt("redis.min_free_conn"),
+			DialTimeout:  1 * time.Second,
+			ReadTimeout:  1 * time.Second,
+			WriteTimeout: 1 * time.Second,
+		})
+	})
+	return redisManageCache
+}
